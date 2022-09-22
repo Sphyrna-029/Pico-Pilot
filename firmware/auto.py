038e35c82d581577e53f4e6c76743f74cd112294
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2022-09-22 05:01:29 -0400

    Update auto.py

--- firmware/auto.py
+++ firmware/auto.py
@@ -1,13 +1,24 @@
 from machine import Pin, UART, I2C
 from ssd1306 import SSD1306_I2C
-import utime, time
 from imu import MPU6050
+import utime, time
+import _thread
+import json
 
-i2c = I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+#Instantiate our static objects
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 oled = SSD1306_I2C(128, 64, i2c)
 gpsModule = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+telemetry = UART(0, baudrate=9600, tx=Pin(12), rx=Pin(13))
 imu = MPU6050(i2c)
 
+#Open our vehicle config
+with open("vehicle.config") as configfile:
+    vehicleConfig = json.load(configfile)
+    configfile.close()
+    print("Config loaded!")
+
+#Perform self health check
 def post():
     oled.fill(0)
     oled.text("POST", 0, 0)
@@ -21,7 +32,6 @@ def post():
         oled.text("POST -", 0, 0)
         oled.text("I2c bus error.", 0, 30) 
         oled.show()
-        
         return False
     
     else:
@@ -31,35 +41,35 @@ def post():
         oled.text(str(devices), 0, 40)
         oled.show()
         time.sleep(5)
-        
         return True
-    
-selfcheck = post()
 
-if not selfcheck:
-    oled.fill(0)
-    oled.text("POST -", 0, 0)
-    oled.text("POST failed.", 0, 30)
-    oled.text("Please check", 0, 40)
-    oled.text("device.", 0, 50)
-    oled.show()
-    time.sleep(2)
+#Determine if POST is required
+if vehicleConfig["PowerOnSelfTest"]:
+    selfcheck = post()
+
+    if not selfcheck:
+        oled.fill(0)
+        oled.text("POST -", 0, 0)
+        oled.text("POST failed.", 0, 30)
+        oled.text("Please check", 0, 40)
+        oled.text("device.", 0, 50)
+        oled.show()
+        time.sleep(2)
     
 
+#GPS Vars
 buff = bytearray(255)
-
-TIMEOUT = False
 FIX_STATUS = False
-
 latitude = ""
 longitude = ""
 satellites = ""
 GPStime = ""
 
-def getGPS(gpsModule):
-    global FIX_STATUS, TIMEOUT, latitude, longitude, satellites, GPStime
+
+#GPS Handler Function
+def getGPS():
+    global FIX_STATUS, latitude, longitude, satellites, GPStime
     
-    timeout = time.time() + 30 
     while True:
         gpsModule.readline()
         buff = str(gpsModule.readline())
@@ -78,14 +88,16 @@ def getGPS(gpsModule):
                 satellites = parts[7]
                 GPStime = parts[1][0:2] + ":" + parts[1][2:4] + ":" + parts[1][4:6]
                 FIX_STATUS = True
-                break
                 
-        if (time.time() > timeout):
-            TIMEOUT = True
-            break
+        else:
+            FIX_STATUS = False
+            
         utime.sleep_ms(500)
 
+#Gather IMU Data 
 def getImu():
+    data = []
+    
     ax=round(imu.accel.x,2)
     ay=round(imu.accel.y,2)
     az=round(imu.accel.z,2)
@@ -93,11 +105,19 @@ def getImu():
     gy=round(imu.gyro.y)
     gz=round(imu.gyro.z)
     tem=round(imu.temperature,2)
-    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
+    
+    data.append(ax)
+    data.append(ay)
+    data.append(az)
+    data.append(gx)
+    data.append(gy)
+    data.append(gz)
+    data.append(tem)
+
+    return data
 
 
 def convertToDegree(RawDegrees):
-
     RawAsFloat = float(RawDegrees)
     firstdigits = int(RawAsFloat/100) 
     nexttwodigits = RawAsFloat - float(firstdigits*100) 
@@ -106,35 +126,25 @@ def convertToDegree(RawDegrees):
     Converted = '{0:.6f}'.format(Converted) 
     return str(Converted)
     
-    
+#Instaniate GPS Thread
+_thread.start_new_thread(getGPS, ())
+
+#Main thread
 while True:
     
-    getGPS(gpsModule)
-    
-    if(FIX_STATUS == True):
-        print("Printing GPS data...")
-        print(" ")
-        print("Latitude: "+latitude)
-        print("Longitude: "+longitude)
-        print("Satellites: " +satellites)
-        print("Time: "+GPStime)
-        print("----------------------")
-        print(getImu())
+    print("Latitude: "+latitude)
+    print("Longitude: "+longitude)
+    print("Satellites: " +satellites)
+    print("Time: "+GPStime)
+    print("----------------------")
+    print(getImu())
         
-        oled.fill(0)
-        oled.text("----------------------", 0, 0)
-        oled.text("Lat: "+latitude, 0, 20)
-        oled.text("Lng: "+longitude, 0, 30)
-        oled.text("Satellites: "+satellites, 0, 40)
-        oled.text("Time: "+GPStime, 0, 50)
-        oled.show()
+    oled.fill(0)
+    oled.text("----------------------", 0, 0)
+    oled.text("La: "+latitude, 0, 20)
+    oled.text("Lo: "+longitude, 0, 30)
+    oled.text("Sat: "+satellites, 0, 40)
+    oled.text("T: "+GPStime, 0, 50)
+    oled.show()
         
-        FIX_STATUS = False
-        
-    if(TIMEOUT == True):
-        oled.fill(0)
-        oled.text("Waiting for GPS fix...", 0, 30)
-        oled.show()
-        print("No GPS data is found.")
-        print(getImu())
-        TIMEOUT = False
+    time.sleep(vehicleConfig["UpdateFrequencyHz"])
